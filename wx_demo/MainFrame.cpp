f5e521d2b6491868525055ed7489212a661b77fa
normalkza <2837097659@QQ.com>
2026-09-17 19:59:56 +0800

    本次更新了toolbar模块和menubar模块,同时对相关代码做了小幅度注释,所需要的图片也已一并打包上传

--- wx_demo/MainFrame.cpp
+++ wx_demo/MainFrame.cpp
@@ -1,20 +1,32 @@
 #include "MainFrame.h"
+#include "MenuBar.h"
+#include "ToolBar.h"
 
 MainFrame::MainFrame()
     : wxFrame(
         nullptr,
         wxID_ANY,
-        "Circuit Editor",
+        wxT("数字电路编辑器"),
         wxDefaultPosition,
         wxSize(1200, 800)
     )
 {
-	wxPanel* panel = new wxPanel(this);//这是覆盖整个窗口的面板
-	wxPanel* toolbox= new wxPanel(panel, wxID_ANY, wxDefaultPosition, wxSize(200, 800));//左侧工具箱面板
-	wxPanel* canvas = new wxPanel(panel);//右侧画布面板,不设置大小,自适应工具箱大小
+    // MenuBar和ToolBar模块加载   
+    SetMenuBar(CreateMainMenuBar());
+    CreateMainToolBar(this);
     
+    // 这一部分是panel函数的初始化,
+    // 创建了一个覆盖整个窗口的面板panel,
+    // 在panel上创建了两个子面板toolbar和canvas,
+    // 并使用wxBoxSizer管理它们的布局
+    wxPanel* panel = new wxPanel(this);//这是覆盖整个窗口的面板
+    wxPanel* toolbox = new wxPanel(panel);//左侧工具箱面板
+    wxPanel* canvas = new wxPanel(panel);//右侧画布面板,不设置大小,自适应工具箱大小
+
     wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);//创建sizer管理区域大小
-	sizer->Add(toolbox, 0, wxEXPAND);//添加工具箱面板到sizer,不随窗口大小变化
-	sizer->Add(canvas, 1, wxEXPAND);//添加画布面板到sizer,随窗口大小变化
-	panel->SetSizer(sizer);//设置sizer管理面板大小
+
+    toolbox->SetMinSize(wxSize(220, -1));
+    sizer->Add(toolbox, 0, wxEXPAND);//添加工具箱面板到sizer,工具箱在水平方向上不参与剩余空间的比例分配。
+    sizer->Add(canvas, 1, wxEXPAND);//添加画布面板到sizer,随窗口大小变化
+    panel->SetSizer(sizer);//设置sizer管理面板大小
 }
--- wx_demo/MyApp.cpp
+++ wx_demo/MyApp.cpp
@@ -1,9 +1,10 @@
 #include <wx/wx.h>
 #include"MyApp.h"
-#include"MainFrame.h""
+#include "MainFrame.h"
 
 bool MyApp::OnInit()
 {
+	wxInitAllImageHandlers();
 	// Create the main application window
 	MainFrame* frame = new MainFrame();
 	// Show the window
